c8d3d198baba431cdd10ab015f70ddd20d80cf64
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-07 14:07:26 -0800

    added html elements and search params

--- animals/animals.js
+++ animals/animals.js
@@ -1,2 +1,11 @@
 // console.log('testing');
+import { animals } from '../data.js';
+import { findById } from '../utils.js';
+
+const name = document.getElementById('animal-name');
+const img = document.getElementById('animal-image');
+const says = document.getElementById('saying');
+
+const params = new URLSearchParams (window.location.search);
+const animal = findById(params.get('id'), animals);
 
